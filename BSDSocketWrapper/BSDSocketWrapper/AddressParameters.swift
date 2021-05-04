0a19a39600cfb72c9521479adacea3e094ccce82
Roman Kozko <romanchek_bananchek@mail.ru>
2021-05-04 12:56:32 +0300

    add parameter's wrappers

---
+++ BSDSocketWrapper/BSDSocketWrapper/AddressParameters.swift
@@ -0,0 +1,37 @@
+//
+//  AddressParameters.swift
+//  BSDSocketWrapper
+//
+//  Created by Admin on 03.05.2021.
+//
+
+import Foundation
+
+enum AddressFamily : Int32{
+    
+    case unspecified = 0  //AF_UNSPEC
+    case IPv4 = 2 //AF_INET
+    case IPv6 = 30 //AF_INET6
+    
+    func getSize()->UInt32{
+        switch self {
+        case .IPv4:
+            return socklen_t(MemoryLayout<sockaddr_in>.size)
+        case .IPv6:
+            return socklen_t(MemoryLayout<sockaddr_in6>.size)
+        case .unspecified:
+            return 0
+        }
+    }
+}
+
+enum SockType : Int32 {
+    case stream = 1    //SOCK_STREAM
+    case datagram = 2  //SOCK_DGRAM
+}
+
+enum Flags : Int32{
+    case passive = 1 //AI_PASSIVE get address to use bind()
+    case `default` = 1536 //AI_DEFAULT
+}
+
