4c116dce9b732f10bd785fc092c47a706d7a130c
Roman Kozko <romanchek_bananchek@mail.ru>
2021-05-05 15:10:57 +0300

    add access to address

--- BSDSocketWrapper/BSDSocketWrapper/SocketAddress.swift
+++ BSDSocketWrapper/BSDSocketWrapper/SocketAddress.swift
@@ -18,13 +18,13 @@ extension SockAddress{
     init(from addrInfo : addrinfo) {
         switch addrInfo.ai_family {
         case AF_INET:
-            let address = addrInfo.ai_addr.withMemoryRebound(to: sockaddr_in.self , capacity: Int(AddressFamily.IPv4.getSize()), {(
+            let address = addrInfo.ai_addr.withMemoryRebound(to: sockaddr_in.self , capacity: 1, {(
                 pointer:UnsafeMutablePointer<sockaddr_in>) in
                 return pointer.pointee
             })
             self = .IPv4(address: address)
         case AF_INET6:
-            let address = addrInfo.ai_addr.withMemoryRebound(to: sockaddr_in6.self , capacity: Int(AddressFamily.IPv6.getSize()), {
+            let address = addrInfo.ai_addr.withMemoryRebound(to: sockaddr_in6.self , capacity: 1, {
                 (pointer: UnsafeMutablePointer<sockaddr_in6>) in
                 return pointer.pointee
             })
@@ -32,6 +32,32 @@ extension SockAddress{
             
         default:
             self = .empty
+        }
+    }
+}
+
+extension SockAddress{
+    func getAddress(params : (UnsafePointer<sockaddr>,socklen_t) throws -> ()) rethrows{
+        switch self {
+        case .IPv4(let address):
+            var addr = withUnsafePointer(to: address, { pointer -> sockaddr in
+                pointer.withMemoryRebound(to: sockaddr.self, capacity: 1, { addrPointer in
+                    return addrPointer.pointee
+                })
+            })
+            try params(&addr,AddressFamily.IPv4.getSize())
+            
+        case .IPv6(let address):
+            var addr = withUnsafePointer(to: address, { pointer -> sockaddr in
+                pointer.withMemoryRebound(to: sockaddr.self, capacity: 1, { addrPointer in
+                    return addrPointer.pointee
+                })
+            })
+            try params(&addr,AddressFamily.IPv6.getSize())
+            
+            
+        case .empty:
+            return
         }
     }
 }
