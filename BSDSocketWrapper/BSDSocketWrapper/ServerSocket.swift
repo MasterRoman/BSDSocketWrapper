cb8fd0455ec4f59aec9c8c35771c3e58af5be34e
Roman Kozko <romanchek_bananchek@mail.ru>
2021-05-06 00:49:07 +0300

    implement server's base functional

--- BSDSocketWrapper/BSDSocketWrapper/ServerSocket.swift
+++ BSDSocketWrapper/BSDSocketWrapper/ServerSocket.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+protocol ServerSocket : BaseSocket {
+}
+
+extension ServerSocket{
+    init(port: String) throws {
+        self = try AddressInfo(host: nil, port: port).getAddressInfo(params: { addrInfo in
+            return try Self.init(addrInfo: addrInfo)
+        })
+    }
+}
+
+
+extension ServerSocket{
+    func listen(with backlog : Int32 = 5) throws{
+        try socket.listen(with: backlog)
+    }
+    
+    func accept<T>() throws -> T where T : BaseSocket{
+        let (clientSocket,address) = try socket.accept()
+        return try T(socket: clientSocket, address: address)
+    }
+}
