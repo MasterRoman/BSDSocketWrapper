711146001106b635805619f3b58f15217c8c5228
Roman Kozko <romanchek_bananchek@mail.ru>
2021-05-08 01:36:46 +0300

    add udp ability of sending and receiving

--- BSDSocketWrapper/BSDSocketWrapper/BaseSocket.swift
+++ BSDSocketWrapper/BSDSocketWrapper/BaseSocket.swift
@@ -76,8 +76,66 @@ extension BaseSocket{
         pointer.deallocate()
         
     }
+}
+
+
+//for UDP
+extension BaseSocket{
+    private func sendTo(buffer : UnsafeBufferPointer<UInt8>) throws {
+        let bytesToSend = buffer.count
+        var sentBytes = 0
+        while sentBytes < bytesToSend {
+            try address.getAddress { (address, length) in
+                sentBytes += try socket.send(to: address, pointer: buffer.baseAddress! + sentBytes, count: buffer.count - sentBytes, sockLength: length)
+            }
+            
+        }
+    }
     
+    func sendTo(message : String) throws{
+        var tempMessage = message
+        try tempMessage.withUTF8({
+            try sendTo(buffer: $0)
+        })
+    }
     
+    func sendTo(data : Data) throws{
+        try data.withUnsafeBytes({ pointer in
+            let typedPointer = pointer.bindMemory(to: UInt8.self)
+            let buffer = UnsafeBufferPointer<UInt8>(start: typedPointer.baseAddress!, count: typedPointer.count)
+            try sendTo(buffer: buffer)
+        })
+    }
+}
+
+extension BaseSocket{
+    func receiveFrom(completionHandler:(String) -> ()) throws{
+        var output = String()
+        
+        let bufferSize = 1024
+        let pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+        pointer.initialize(repeating: 0, count: bufferSize)
+        
+        let buffer = UnsafeMutableBufferPointer(start: pointer, count: bufferSize)
+        
+        try address.getMutableAddress { (address,length) in
+            var recivedBytes = 0
+            repeat {
+                recivedBytes = try socket.receive(from: address, buffer: buffer, sockLength:length)
+                guard recivedBytes != 0 else {
+                    break
+                }
+                let string = String(cString: buffer.baseAddress!)
+                output.append(string)
+            } while true
+            
+        }
+        
+        
+        completionHandler(output)
+        pointer.deinitialize(count: bufferSize)
+        pointer.deallocate()
+    }
 }
 
 extension BaseSocket{
